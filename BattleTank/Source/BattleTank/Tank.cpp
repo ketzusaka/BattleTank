4e2d650d507f202c65559ca3645a6d0cc8ccee9f
James Richard <ketzu@me.com>
2016-08-28 20:25:10 -0700

    Little more forward declaration cleanup

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankAimingComponent.h"
 #include "Tank.h"
-
 
 // Sets default values
 ATank::ATank()
